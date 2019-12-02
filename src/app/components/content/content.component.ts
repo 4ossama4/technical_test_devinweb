6ec9964a0843fe5f442259ead39e5b4c5e62a34a
Ossama <ossama.attaleb@gmail.com>
2019-12-02 21:43:42 +0100

    update text , pictures articles

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -17,7 +17,7 @@ export class ContentComponent implements OnInit {
 		this.articleservice.getArticles().subscribe
 			(reponse => {
 				this.articles=reponse;
-				this.articleRocmmended=this.articles.filter(article => article.rate > 0);
+				this.articleRocmmended=	this.articles.sort((a,b)=>{return b.rate - a.rate}).filter(article => article.rate > 0);
 				this.articleCategoryA=this.articles.filter(article => article.category == 'A');
 			}
 			);
--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,6 +1,7 @@
 export class Article{
 
     constructor(
+        public id:number,
         public title: String,
         public content: String,
         public author: String,
